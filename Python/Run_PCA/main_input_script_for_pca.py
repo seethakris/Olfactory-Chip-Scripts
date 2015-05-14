9185d556adb1deba5ba4b9d0134ffe7b3447a31c
seethakris <seethakris@gmail.com>
2015-05-14 11:13:02 +0800

    Changes to stimulus on and off

--- Python/Run_PCA/main_input_script_for_pca.py
+++ Python/Run_PCA/main_input_script_for_pca.py
@@ -8,20 +8,19 @@ Created on Mon Jan 26 17:07:18 2015
 
 
 ## Enter Main Folder containing stimulus folders to create text files
-Exp_Folder ='/Users/seetha/Desktop/KCTD/Fish14_KCTDHUC_5dpf/Tiff/Cropped/Registered/Thresholded_OB/Registered_Stimulus/'
-filename_save_prefix_forPCA = 'ThresholdedOB_1and2PC_T81'
-filename_save_prefix_for_textfile = 'ThresholdedOB_T81'
+Exp_Folder ='/Users/seetha/Desktop/KCTD/Fish24_KCTDHUC_9dpf/Tiff/Cropped/Registered/Thresholded_OB/Registered_Stimulus/'
+filename_save_prefix_forPCA = 'ThresholdedOB_12and3_T129'
+filename_save_prefix_for_textfile = 'ThresholdedOB_T129'
 #Which files to do PCA on
 files_to_do_PCA = [0,0,1] #Individual PCA, Each_odor PCA, All_odor PCA
 
 #Use existing parameters from pickle dump -1  or use new paprameters -0?
-use_existing_parameters = 1
+use_existing_parameters = 0
 
 #Redo pca - 1
-redo_pca = 1
-
+redo_pca = 0
 # Required pcs from what was received previously
-required_pcs = [1,2]
+required_pcs = 0
 
 time_baseline = 10
 
@@ -47,18 +46,6 @@ thresh_pca_allodor = 0.000001 #Threshold above which to plot the pca components
 color_map_allodor= 'polar' #Colormap for plotting principle components
 
 
-#Stimulus on and off time
-stimulus_pulse = 1
-if stimulus_pulse == 1:
-    stimulus_on_time = [10,30,48,67]
-    stimulus_off_time = [14,34,52,71]
-    color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF']
-
-    
-elif stimulus_pulse == 2:
-    stimulus_on_time = [10,29,48,67,86,105]
-    stimulus_off_time = [13,32,51,70,89,108]
-    color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF', '#800000','#A52A2A']
 
 ######################################################################
 ########################## Run Scripts ###############################
@@ -77,7 +64,7 @@ if use_existing_parameters == 1:
         pca_components_allodor, num_pca_colors_allodor, num_samples_allodor, thresh_pca_allodor, color_map_allodor,required_pcs  = pickle.load(f)
 
 #Stimulus on and off time
-stimulus_pulse = 1
+stimulus_pulse = 5
 if stimulus_pulse == 1:
     stimulus_on_time = [10,30,48,67]
     stimulus_off_time = [14,34,52,71]
@@ -85,10 +72,24 @@ if stimulus_pulse == 1:
 
     
 elif stimulus_pulse == 2:
-    stimulus_on_time = [10,29,48,67,86,105]
-    stimulus_off_time = [13,32,51,70,89,108]
+    stimulus_on_time = [10,28,47,65,84,101]
+    stimulus_off_time = [13,31,52,68,87,104]
+    color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF', '#800000','#A52A2A']
+
+elif stimulus_pulse == 3: #Fish 21 onwards
+    stimulus_on_time = [10,29,48,66,85,103]
+    stimulus_off_time = [13,32,53,69,88,106]
     color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF', '#800000','#A52A2A']
     
+elif stimulus_pulse == 4: #Fish 23 onwards
+    stimulus_on_time = [10,28,46,65,83,102]
+    stimulus_off_time = [13,31,51,70,86,105]
+    color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF', '#800000','#A52A2A']
+
+elif stimulus_pulse == 5: #Only Fish 24 
+    stimulus_on_time = [10,29,46,66,84,104]
+    stimulus_off_time = [13,32,51,69,87,107]
+    color_mat = ['#00FFFF','#0000A0','#800080','#FF00FF', '#800000','#A52A2A']
 # Go into the main function that does pca for indiviudal trials
 from pca_thunder_analysis import run_analysis_individualodors
 from pca_thunder_analysis import run_analysis_eachodor
